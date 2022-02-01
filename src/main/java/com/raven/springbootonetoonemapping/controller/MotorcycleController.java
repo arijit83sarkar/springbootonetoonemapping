bc64509ea80e8b3650516de5f3e08e955b3a76a9
arijit83sarkar <mail2arijit83@gmail.com>
2022-02-02 00:43:46 +0530

    code pushed in git

---
+++ src/main/java/com/raven/springbootonetoonemapping/controller/MotorcycleController.java
@@ -0,0 +1,34 @@
+package com.raven.springbootonetoonemapping.controller;
+
+import com.raven.springbootonetoonemapping.entiry.Motorcycle;
+import com.raven.springbootonetoonemapping.repository.MotorcycleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/motorcycle")
+public class MotorcycleController {
+
+    @Autowired
+    private MotorcycleRepository motorcycleRepository;
+
+    @PostMapping("/saveMotorcycle")
+    public Motorcycle saveMotorcycle(@RequestBody Motorcycle motorcycle) {
+        System.out.println("Motorcycle save called...");
+        Motorcycle outMotorcycle = motorcycleRepository.save(motorcycle);
+        System.out.println("Saved Motorcycle :: " + outMotorcycle);
+
+        return outMotorcycle;
+    }
+
+    @GetMapping("/getMotorcycle/{id}")
+    public Optional<Motorcycle> getMotorcycle(@PathVariable String id) {
+        System.out.println("Motorcycle get() called...");
+        Optional<Motorcycle> outMotorcycle = motorcycleRepository.findById(Integer.valueOf(id));
+        System.out.println("Motorcycle with Engine :: " + outMotorcycle);
+
+        return outMotorcycle;
+    }
+}
